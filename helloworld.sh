1a0c240b14473027a1301638559f1ef8861b980b
justin-eelma <eelmajustin@gmail.com>
2022-07-26 23:01:46 -0700

    Edited Code

--- helloworld.sh
+++ helloworld.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Justin Eelma
+# Date of latest revision: 7-26-22
+# Purpose: Print a string to the terminal
+
+# Main
+
+echo "I am working perfectly"
+
+# End
