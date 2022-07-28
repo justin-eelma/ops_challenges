8acac5b3ea0a59c3eb9d5841aa3385e4b97a3f1d
justin-eelma <109092762+justin-eelma@users.noreply.github.com>
2022-07-28 15:09:58 -0700

    Create arraypractice.sh

---
+++ arraypractice.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 04 Ops Challenge
+# Author: Justin Eelma
+# Date of last revision: 7/28/22
+# Purpose: Create four directories with sample files in each directory
+
+# main
+# create dir{1...4}
+
+mkdir dir1 dir2 dir3 dir4
+
+dirs=(dir1 dir2 dir3 dir4)
+
+touch ${dirs[0]}/txt1.txt
+touch ${dirs[1]}/txt2.txt
+touch ${dirs[2]}/txt3.txt
+touch ${dirs[3]}/txt4.txt
+
+# feedback message
+
+echo "Your new directories:"
+ls ${dirs[@]}
+
+# end
