3d6e31b0ebae7112c1ef2fa92c4ac88d81b78c21
justin-eelma <109092762+justin-eelma@users.noreply.github.com>
2022-08-08 19:38:01 -0700

    Create Ops11.ps1

---
+++ Ops11.ps1
@@ -0,0 +1,22 @@
+# Script: Ops 201 Class 11 Ops Challenge
+# Author: Justin Eelma
+# Date of last revision: 8/8/22
+# Purpose: Write a Powershell script that automates the configuration of a new Windows 10 endpoint.
+
+# Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Enabled True
+
+# Allow ICMP traffic
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
+
+# Enable Remote management
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+# Remove bloatware
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+
+# Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# Disable SMBv1, an insecure protocol
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
